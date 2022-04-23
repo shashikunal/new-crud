a180ef0bd97abb41dd221dfb2e246b7b842dc5ac
shashikunal <shashikunal@gmail.com>
2022-04-23 13:16:35 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/navbar/Navbar";
+import CreatePost from "./components/posts/CreatePost";
+import Posts from "./components/posts/Posts";
+import EditPost from "./components/posts/EditPost";
+import DeletePost from "./components/posts/DeletePost";
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Posts />} />
+        <Route path="/create-post" element={<CreatePost />} />
+        <Route path="/edit-post/:id" element={<EditPost />} />
+        <Route path="/delete-post/:id" element={<DeletePost />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
